59dd2abc3770cb0113eeeaa29fcd775fdee19e9d
Tahmid Zishan <tahmidzishan53@gmail.com>
2026-04-13 21:13:23 +0600

    contest1 h

--- contest1/h.cpp
+++ contest1/h.cpp
@@ -1,15 +1,15 @@
+// Author: Tahmid Zishan
 #include <iostream>
 using namespace std;
 
 int main(){
-    double n, k, a;
+    unsigned long long n, k, a;
     cin >> n >> k >> a;
-    // cout << n << ' ' << k << ' ' << a << endl;
-    float meh = (n * k) / a;
-    // //
-    // if (meh <= 2147483647) cout << "int";
-    // else if (meh % 1 > 0) cout << "double";
-    // else cout << "long long";
-    cout << typeid((n * k) / a).name() << endl;
+
+    // if there's a remainder for either n or k, the result should be a float (double)
+    if (((n % a) * (k % a)) % a) cout << "double";
+    // if its not a double, its either int or long long
+    else if ((n * k) / a > 2147483647) cout << "long long";
+    else cout << "int";
     return 0;
 }
